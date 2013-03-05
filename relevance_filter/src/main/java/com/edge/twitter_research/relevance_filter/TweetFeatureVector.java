23e55b1481a7da5ea88cdae1f632480641d17aee
Mo Luo <mo.luo1@yahoo.com>
2013-03-05 18:02:35 -0500

    Added a new class to represent a tweet's feature vector for the relevance filter

---
+++ relevance_filter/src/main/java/com/edge/twitter_research/relevance_filter/TweetFeatureVector.java
@@ -0,0 +1,18 @@
+package com.edge.twitter_research.relevance_filter;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Mo
+ * Date: 3/5/13
+ * Time: 5:23 PM
+ * To change this template use File | Settings | File Templates
+ */
+
+import com.edge.twitter_research.core.*;
+
+public class TweetFeatureVector {
+
+    public TweetFeatureVector(SimpleTweet simpleTweet){
+
+    }
+}
