680c926404eaa4895dc7cc989c16d5b7f4bbae98
Mo Luo <mo.luo1@yahoo.com>
2013-03-11 16:43:22 -0400

    Added a basic feature vector implementation

--- relevance_filter/src/main/java/com/edge/twitter_research/relevance_filter/TweetFeatureVector.java
+++ relevance_filter/src/main/java/com/edge/twitter_research/relevance_filter/TweetFeatureVector.java
@@ -12,7 +12,66 @@ import com.edge.twitter_research.core.*;
 
 public class TweetFeatureVector {
 
-    public TweetFeatureVector(SimpleTweet simpleTweet){
+    long[] feature_vector;
 
+    public TweetFeatureVector(SimpleTweet simpleTweet){
+        feature_vector = new long[17];
+        feature_vector[0] = simpleTweet.getId();
+        if (!simpleTweet.getContributors().isEmpty())
+            feature_vector[1] = 1;
+        String date_info = simpleTweet.getCreatedAt().toString();
+        String weekday = date_info.substring(14, 17);
+        String hour = date_info.substring(25, 27);
+        int time = Integer.parseInt(hour);
+        int date_data = 0;
+        if (((((weekday.equals("Mon")
+                || weekday.equals("Tues"))
+                || weekday.equals("Wed"))
+                ||weekday.equals("Thurs"))
+                ||weekday.equals("Fri")))
+            date_data = 10;
+        else if (weekday.equals("Sat")
+                || weekday.equals("Sun"))
+            date_data = 0;
+        if ((time < 13) && time > 6)
+            date_data += 0;
+        else if ((time >= 13) && time < 18)
+            date_data += 4;
+        else
+            date_data += 9;
+        feature_vector[2] = date_data;
+        if (!simpleTweet.getUrlEntities().isEmpty())
+            feature_vector[3] = 1;
+        //URL position goes here
+        feature_vector[5] = simpleTweet.getHashTagEntities().size();
+        feature_vector[6] = simpleTweet.getUserMentionEntities().size();
+        if (!simpleTweet.getMediaEntities().isEmpty())
+            feature_vector[7] = 1;
+        //filter level
+        if (simpleTweet.getInReplyToStatusId() != null)
+            feature_vector[9] = 1;
+        //place
+        if (simpleTweet.getIsPossiblySensitive())
+            feature_vector[11] = 1;
+        feature_vector[12] = simpleTweet.getRetweetCount();
+        //Source
+        feature_vector[14] = simpleTweet.getText().length();
+        //Emotes
+        //Questions
     }
 }
+
+/*id: long
+contributors: boolean
+created_at: <labelled in 2 dimensions: weekday vs weekend and times of day (morning, afternoon, night)
+entities_url: boolean
+entities_url_position: <labelled as beginning or end>
+entities_hashtags: int
+entities_mentions: int
+entities_media: boolean
+filter_level: <labelled as all the Twitter filter levels>
+is_reply: boolean (derived from any of the “in_reply_*” fields)
+place: boolean
+possibly_sensitive: boolean
+retweeted: int
+source: <labelled as mobile or desktop> derived from list of client names*/
