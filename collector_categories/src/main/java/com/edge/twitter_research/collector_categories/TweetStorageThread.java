a03103647313b249925a0bddbc180e37538cb480
Arpan Ghosh <arpanghosh@gmail.com>
2013-03-14 16:56:13 -0700

    Added SimpleUser and SimplePlace internal objects to SimpleTweet

--- collector_categories/src/main/java/com/edge/twitter_research/collector_categories/TweetStorageThread.java
+++ collector_categories/src/main/java/com/edge/twitter_research/collector_categories/TweetStorageThread.java
@@ -7,10 +7,7 @@ import org.kiji.schema.KijiTable;
 
 import org.apache.log4j.Logger;
 
-import twitter4j.*;
-
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.concurrent.LinkedBlockingQueue;
 
 import  com.edge.twitter_research.core.*;
@@ -64,7 +61,7 @@ public class TweetStorageThread extends Thread {
                                     GlobalConstants.TWEET_COLUMN_FAMILY_NAME,
                                     GlobalConstants.TWEET_COLUMN_NAME,
                                     System.currentTimeMillis(),
-                                    generateSimpleTweet(tweetCategoryMessage.tweet));
+                                    SimpleTweetGenerator.generateSimpleTweet(tweetCategoryMessage.tweet));
                 kijiTableWriter.put(tweetId,
                                     GlobalConstants.TWEET_COLUMN_FAMILY_NAME,
                                     GlobalConstants.LABEL_COLUMN_NAME,
@@ -78,82 +75,6 @@ public class TweetStorageThread extends Thread {
         }
     }
 
-
-    private SimpleTweet generateSimpleTweet(Status status){
-        SimpleTweet simpleTweet = new SimpleTweet();
-
-        ArrayList<Long> contributors = new ArrayList<Long>();
-        for (long contributor : status.getContributors())
-            contributors.add(contributor);
-        simpleTweet.setContributors(contributors);
-
-        simpleTweet.setCreatedAt(status.getCreatedAt().toString());
-        simpleTweet.setCurrentUserRetweetId(status.getCurrentUserRetweetId());
-        simpleTweet.setId(status.getId());
-        simpleTweet.setInReplyToScreenName(status.getInReplyToScreenName());
-        simpleTweet.setInReplyToStatusId(status.getInReplyToStatusId());
-        simpleTweet.setInReplyToUserId(status.getInReplyToUserId());
-        simpleTweet.setRetweetCount(status.getRetweetCount());
-        simpleTweet.setSource(status.getSource());
-        simpleTweet.setText(status.getText());
-        simpleTweet.setUserId(status.getUser().getId());
-        simpleTweet.setIsFavorited(status.isFavorited());
-        simpleTweet.setIsPossiblySensitive(status.isPossiblySensitive());
-        simpleTweet.setIsRetweet(status.isRetweet());
-        simpleTweet.setIsRetweetedByMe(status.isRetweetedByMe());
-        simpleTweet.setIsTruncated(status.isTruncated());
-
-        ArrayList<SimpleHashtagEntity> hashtagEntities =
-                new ArrayList<SimpleHashtagEntity>();
-        for (HashtagEntity hashtagEntity : status.getHashtagEntities()){
-            SimpleHashtagEntity simpleHashtagEntity = new SimpleHashtagEntity();
-            simpleHashtagEntity.setEnd(hashtagEntity.getEnd());
-            simpleHashtagEntity.setStart(hashtagEntity.getStart());
-            simpleHashtagEntity.setText(hashtagEntity.getText());
-            hashtagEntities.add(simpleHashtagEntity);
-        }
-        simpleTweet.setHashTagEntities(hashtagEntities);
-
-        ArrayList<SimpleURLEntity> urlEntities =
-                new ArrayList<SimpleURLEntity>();
-        for (URLEntity urlEntity : status.getURLEntities()){
-            SimpleURLEntity simpleURLEntity = new SimpleURLEntity();
-            simpleURLEntity.setEnd(urlEntity.getEnd());
-            simpleURLEntity.setStart(urlEntity.getStart());
-            simpleURLEntity.setURL(urlEntity.getURL());
-            simpleURLEntity.setDisplayURL(urlEntity.getDisplayURL());
-            simpleURLEntity.setExpandedURL(urlEntity.getExpandedURL());
-            urlEntities.add(simpleURLEntity);
-        }
-        simpleTweet.setUrlEntities(urlEntities);
-
-        ArrayList<SimpleUserMentionEntity> userMentionEntities =
-                new ArrayList<SimpleUserMentionEntity>();
-        for (UserMentionEntity userMentionEntity : status.getUserMentionEntities()){
-            SimpleUserMentionEntity simpleUserMentionEntity = new SimpleUserMentionEntity();
-            simpleUserMentionEntity.setEnd(userMentionEntity.getEnd());
-            simpleUserMentionEntity.setStart(userMentionEntity.getStart());
-            simpleUserMentionEntity.setId(userMentionEntity.getId());
-            simpleUserMentionEntity.setName(userMentionEntity.getName());
-            simpleUserMentionEntity.setScreenName(userMentionEntity.getScreenName());
-            userMentionEntities.add(simpleUserMentionEntity);
-        }
-        simpleTweet.setUserMentionEntities(userMentionEntities);
-
-        ArrayList<SimpleMediaEntity> mediaEntities =
-                new ArrayList<SimpleMediaEntity>();
-        for (MediaEntity mediaEntity : status.getMediaEntities()){
-            SimpleMediaEntity simpleMediaEntity = new SimpleMediaEntity();
-            simpleMediaEntity.setId(mediaEntity.getId());
-            simpleMediaEntity.setType(mediaEntity.getType());
-            simpleMediaEntity.setMediaURL(mediaEntity.getMediaURL());
-            simpleMediaEntity.setMediaURLHttps(mediaEntity.getMediaURLHttps());
-            mediaEntities.add(simpleMediaEntity);
-        }
-        simpleTweet.setMediaEntities(mediaEntities);
-
-        return simpleTweet;
-    }
 
     private boolean timeToStop(TweetCategoryMessage tweetCategoryMessage){
         return (tweetCategoryMessage.category_slug.equals("") &&
