0a0480d54f96b53bbca0319c9774a5f0bb7bb460
Mo Luo <mo.luo1@yahoo.com>
2013-03-29 23:08:57 -0400

    feature vector problems fixed and cleaned up

--- relevance_filter/src/main/java/com/edge/twitter_research/relevance_filter/TweetFeatureVector.java
+++ relevance_filter/src/main/java/com/edge/twitter_research/relevance_filter/TweetFeatureVector.java
@@ -11,130 +11,126 @@ package com.edge.twitter_research.relevance_filter;
 import com.edge.twitter_research.core.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Locale;
 import java.util.regex.Pattern;
 import org.jsoup.Jsoup;
 import java.util.Calendar;
+import java.util.List;
 
 public class TweetFeatureVector {
 
-    long[] feature_vector;
-    long id;
-    int has_contributers;
-    int daytime;
-    int weekday;
-    int has_urls;
-    int url_place;
-    int num_hashtags;
-    int num_mentions;
-    int has_media;
-    int reply;
-    int sensitive;
-    long retweet_count;
-    int text_size;
-    int num_emotes;
-    int question;
-    int isMobile;
+    private static Pattern emote_match = Pattern.compile("(\\Q:)\\E|\\Q:D\\E|\\Q:(\\E|\\Q;)\\E|\\Q:-)\\E|\\Q:P\\E|\\Q=)" +
+            "\\E|\\Q(:\\E|\\Q;-)\\E|\\Q:/\\E|\\QXD\\E|\\Q=D\\E|\\Q=]\\E|\\QD:\\E|\\Q;D\\E|\\Q:]\\E|\\Q:-(\\E|\\Q=/\\E|" +
+            "\\Q:O\\E|\\Q=(\\E|\\Q):\\E|\\Q=P\\E|\\Q:'(\\E|\\Q:|\\E|\\Q:-D\\E|\\Q^_^\\E|\\Q(8\\E|\\Q:-/\\E|\\Q:o)\\E|" +
+            "\\Q:o\\E|\\Q:-P\\E|\\Q(;\\E|\\Q;P\\E|\\Qo:\\E|\\Q;]\\E|\\Q:@\\E|\\Q=[\\E|\\Q:\\\\E|\\Q;(\\E|\\Q:[\\E|\\Q8)" +
+            "\\E|\\Q;o)\\E|\\Q=\\\\E|\\Q=O\\E|\\Q(=\\E|\\Q[:\\E|\\Q;/\\E|\\Q8D\\E|\\Q:}\\E|\\Q\\m/\\E|\\QO:\\E|\\Q/:\\E|" +
+            "\\Q;O\\E|\\Q^-^\\E|\\Q8-)\\E|\\Q=|\\E|\\Q]:\\E|\\QD;\\E|\\Q:o(\\E|\\Q|:\\E|\\Q;-P\\E|\\Q);\\E|\\Q=o\\E|\\Q;-D" +
+            "\\E|\\Q:-\\\\E|\\Q(^_^)\\E|\\Q:-O\\E|\\Q:-o\\E|\\QD=\\E|\\Q(^_^;)\\E|\\Q;o\\E|\\Q;-(\\E|\\Q;@\\E|\\QP:\\E|" +
+            "\\Q@:\\E|\\Q:-|\\E|\\Q[=\\E|\\Q(^-^)\\E|\\Q[8\\E|\\Q(T_T)\\E|\\Q(-_-)\\E|\\Q(-:\\E|\\Q)=\\E|\\Q:{\\E|\\Q=}" +
+            "\\E|\\Q[;\\E|\\Q:?\\E|\\Q8-]\\E|\\Q:*(\\E|\\Qo;\\E|\\QD8\\E|\\Q;}\\E|\\Q;[\\E|\\Q:o/\\E|\\Q:oP\\E|\\Q:-]\\E|" +
+            "\\Q:oD\\E|\\Q8/\\E|\\Q8(\\E|\\Qo(^-^)o\\E)");
+    private static Pattern question_occurances = Pattern.compile(".*?.*");
+    private static Pattern mobileFinder = Pattern.compile("(\\Qobile\\E|\\QiOS\\E|\\Qapp\\E|\\QApp\\E|\\QPlayStation\\E|" +
+            "\\Qokia\\E|\\Qnstag\\E|\\Qnap\\E|\\Qstream\\E|\\Qfon\\E|\\Qnette\\E|\\Qrite\\E|\\Qwicc\\E|\\Qweetlog\\E|" +
+            "\\Qterous\\E|\\Qastin\\E)");
+    private long id;
+    private int daytime;
+    private int weekday;
+    private int url_place;
+    private long retweet_count;
+    private int text_size;
+    private int num_emotes;
+    private int[] t_or_f;
+    private int isMobile;
+    private double[] fractions;
 
     public TweetFeatureVector(SimpleTweet simpleTweet){
         id = simpleTweet.getId();
-        if (!simpleTweet.getContributors().isEmpty())
-            has_contributers = 1;
-        String date_info = simpleTweet.getCreatedAt().toString();
-        Date myDate = get_Date(date_info);
-        Calendar c = Calendar.getInstance();
-        c.setTime(myDate);
-        int day = c.get(Calendar.DAY_OF_WEEK);
-        int time = c.get(Calendar.HOUR_OF_DAY);
-        if(!(day == Calendar.SATURDAY) && !(day == Calendar.SUNDAY))
-            weekday = 1;
-        if(time < 13 && time > 5)
-            daytime = 1;
-        else if(time > 12 && time < 16)
-            daytime = 2;
-        else if(time > 15 && time < 23)
-            daytime = 3;
-        else
-            daytime = 4;
-        if(!simpleTweet.getUrlEntities().isEmpty())
-            has_urls = 1;
-        num_hashtags = simpleTweet.getHashTagEntities().size();
-        num_mentions = simpleTweet.getUserMentionEntities().size();
-        if (!simpleTweet.getMediaEntities().isEmpty())
-            has_media = 1;
+        int[] date = date_info(get_Date(simpleTweet.getCreatedAt().toString()));
+        weekday = date[0];
+        daytime = date[1];
+        fractions = fractions(simpleTweet);
         //filter level... New feature, will probably have to update simpleTweet
-        if (simpleTweet.getInReplyToStatusId() != null)
-            reply = 1;
         //place... A bit problematic. Ignore for now.
-        if (simpleTweet.getIsPossiblySensitive())
-            sensitive = 1;
         retweet_count = simpleTweet.getRetweetCount();
-        //Source
-        text_size = simpleTweet.getText().length();
-        //URL position goes here 1 for front, 2 for center, 3 for back
-        int len = url_length(simpleTweet);
-        int loc = url_loc(simpleTweet);
-        int text = text_size - len;
-        if(((double)loc)/text < 1/5)
-            url_place = 1;
-        else if (((double)loc)/text > 1/5)
-            url_place = 3;
-        else
-            url_place = 2;
-        //Emotes. Generated this giant regex of the Wikipedia list. Not sure if this actually owrks since I don't have enough test data.
         String tweet_text = simpleTweet.getText().toString();
-
-        Pattern emote_match = Pattern.compile("\\Q:)\\E(|\\Q:D\\E|\\Q:(\\E|\\Q;)\\E|\\Q:-)\\E|\\Q:P\\E|\\Q=)\\E|\\Q(:\\E|" +
-                "\\Q;-)\\E|\\Q:/\\E|\\QXD\\E|\\Q=D\\E|\\Q=]\\E|\\QD:\\E|\\Q;D\\E|\\Q:]\\E|\\Q:-(\\E|\\Q=/\\E|\\Q:O\\E|\\Q" +
-                "=(\\E|\\Q):\\E|\\Q=P\\E|\\Q:'(\\E|\\Q:|\\E|\\Q:-D\\E|\\Q^_^\\E|\\Q(8\\E|\\Q:-/\\E|\\Q:o)\\E|\\Q:o\\E|\\Q" +
-                ":-P\\E|\\Q(;\\E|\\Q;P\\E|\\Qo:\\E|\\Q;]\\E|\\Q:@\\E|\\Q=[\\E|\\Q:\\\\E|\\Q;(\\E|\\Q:[\\E|\\Q8)\\E|\\Q;o)" +
-                "\\E|\\Q=\\\\E|\\Q=O\\E|\\Q(=\\E|\\Q[:\\E|\\Q;/\\E|\\Q8D\\E|\\Q:}\\E|\\Q\\m/\\E|\\QO:\\E|\\Q/:\\E|\\Q;O\\E" +
-                "|\\Q^-^\\E|\\Q8-)\\E|\\Q=|\\E|\\Q]:\\E|\\QD;\\E|\\Q:o(\\E|\\Q|:\\E|\\Q;-P\\E|\\Q);\\E|\\Q=o\\E|\\Q;-D\\E|" +
-                "\\Q:-\\\\E|\\Q(^_^)\\E|\\Q:-O\\E|\\Q:-o\\E|\\QD=\\E|\\Q(^_^;)\\E|\\Q;o\\E|\\Q;-(\\E|\\Q;@\\E|\\QP:\\E|\\Q@:" +
-                "\\E|\\Q:-|\\E|\\Q[=\\E|\\Q(^-^)\\E|\\Q[8\\E|\\Q(T_T)\\E|\\Q(-_-)\\E|\\Q(-:\\E|\\Q)=\\E|\\Q:{\\E|\\Q=}\\E" +
-                "|\\Q[;\\E|\\Q:?\\E|\\Q8-]\\E|\\Q:*(\\E|\\Qo;\\E|\\QD8\\E|\\Q;}\\E|\\Q;[\\E|\\Q:o/\\E|\\Q:oP\\E|\\Q:-]\\E" +
-                "|\\Q:oD\\E|\\Q8/\\E|\\Q8(\\E|\\Qo(^-^)o\\E)");
-        String[] no_emotes = emote_match.split(tweet_text);
-        num_emotes = no_emotes.length - 1;
+        t_or_f = booleans(simpleTweet, tweet_text);
+        text_size = tweet_text.length();
+        url_place = url_val(simpleTweet.getUrlEntities().get(0), text_size);
+        //Emotes. Generated this giant regex of the Wikipedia list. Not sure if this actually owrks since I don't have enough test data.
+        num_emotes = emoteCount(tweet_text);
         //Questions
-        Pattern question_occurances = Pattern.compile(".*?.*");
-        String space_stripped = tweet_text.replaceAll("\\s", "");
-        String[] questions = question_occurances.split(tweet_text);
-        question = 0;
-        for(String segment : questions){
-            if(!segment.matches(""))
-                question++;
-        }
-        if (question>0)
-            question--;
+        isMobile = mobile(simpleTweet.getSource().toString());
 
-        String source = simpleTweet.getSource().toString();
-        String clean_source = Jsoup.parse(source).text();
+
+    }
+
+    private static int url_val(SimpleURLEntity url, int len){
+        int[] loc = url_loc(url);
+        int length = len - loc[1] + loc[0] + 1;
+        if(loc[0] == 0)
+            return 1;
+        else if(loc[0] == length - 1)
+            return 2;
+        return 0;
+    }
+
+    private static int[] booleans(SimpleTweet tweet, String str){
+        int[] values = new int[4];
+        if (!tweet.getContributors().isEmpty())
+            values[0] = 1;
+        if (tweet.getInReplyToStatusId() != null)
+            values[1] = 1;
+        if (tweet.getIsPossiblySensitive())
+            values[2] = 1;
+        if(str.contains("?"))
+            values[3] = 1;
+        return values;
+    }
+
+    private static int mobile(String str){
+        String clean_source = Jsoup.parse(str).text();
         clean_source = clean_source.replaceAll("\\s", "");
-        if ((clean_source.contains("for") && !clean_source.contains("forMac")) || clean_source.contains("obile")
-                                    || clean_source.contains("iOS") || clean_source.contains("app")
-                                    || clean_source.contains("App") || clean_source.contains("PlayStation")
-                                    || clean_source.contains("Nokia") || clean_source.contains("Insta")
-                                    || clean_source.contains("nap") || clean_source.contains("tream"))
-            isMobile = 1;
-
-
+        if ((clean_source.contains("for") && !clean_source.contains("forMac")) || (mobileFinder.split(str).length > 1))
+            return 0;
+        return 1;
     }
 
-    private static int url_length(SimpleTweet tweet){
-        int[] url_loc = {tweet.getUrlEntities().get(0).getStart(), tweet.getUrlEntities().get(0).getEnd()};
-        int url_len = url_loc[1] - url_loc[0];
-        return url_len;
+    private static int emoteCount(String str){
+        return emote_match.split(str).length - 1;
     }
-    private static int url_loc(SimpleTweet tweet){
-        int[] url_loc = {tweet.getUrlEntities().get(0).getStart(), tweet.getUrlEntities().get(0).getEnd()};
-        return url_loc[0];
+
+    private static double[] fractions(SimpleTweet tweet){
+        double urltot = 0;
+        double hashtot = 0;
+        double menttot = 0;
+        double medtot = 0;
+        List<SimpleURLEntity> urls = tweet.getUrlEntities();
+        for (SimpleURLEntity url : urls)
+            urltot += (url_loc(url)[1] - url_loc(url)[1] + 1);
+        List<SimpleHashtagEntity> hashes = tweet.getHashTagEntities();
+        for (SimpleHashtagEntity hash : hashes)
+            hashtot += hash.getText().length() + 1;
+        List<SimpleUserMentionEntity> mentions = tweet.getUserMentionEntities();
+        for (SimpleUserMentionEntity mention : mentions)
+            menttot += mention.getScreenName().length() + 1;
+        List<SimpleMediaEntity> media = tweet.getMediaEntities();
+        for(SimpleMediaEntity medium : media)
+            medtot += medium.getMediaURL().length();
+        urltot -= medtot;
+        int len = tweet.getText().length();
+        double[] parts = {urltot/len, hashtot/len, menttot/len, medtot/len};
+        return parts;
+    }
+
+    private static int[] url_loc(SimpleURLEntity url){
+        int[] url_loc = {url.getStart(), url.getEnd()};
+        return url_loc;
     }
 
 
     private static Date get_Date(String str){
         try{
-        Date myDate = new SimpleDateFormat("EEE dd/MM/yyyy").parse(str);
+        Date myDate = new SimpleDateFormat("EEE dd MM KK:mm:ss ZZZZZ yyyy").parse(str);
             return myDate;
         }
         catch(Exception e){
@@ -143,29 +139,51 @@ public class TweetFeatureVector {
         return null;
     }
 
+    private static int[] date_info(Date myDate){
+        int[] info = new int[2];
+        Calendar c = Calendar.getInstance();
+        c.setTime(myDate);
+        int day = c.get(Calendar.DAY_OF_WEEK);
+        int time = c.get(Calendar.HOUR_OF_DAY);
+        if(!(day == Calendar.SATURDAY) && !(day == Calendar.SUNDAY))
+            info[0] = 1;
+        if(time < 13 && time > 5)
+            info[1] = 1;
+        else if(time > 12 && time < 16)
+            info[1] = 2;
+        else if(time > 15 && time < 23)
+            info[1] = 3;
+        else
+            info[1] = 4;
+        return info;
+    }
+
     public long id(){
         return id;
     }
 
     public String toString(){
-        return "" + id + " " + has_contributers + " " + daytime + " " + weekday + " " + has_urls + " " + url_place
-                + " " + num_hashtags + " " + num_mentions + " " + has_media + " " + reply + " " + sensitive + " " +
-                retweet_count + " " + text_size + " " + num_emotes + " " + question + " " + isMobile;
+        StringBuilder featureVector = new StringBuilder();
+        featureVector.append(id);
+        featureVector.append("|");
+        featureVector.append(daytime);
+        featureVector.append("|");
+        featureVector.append(weekday);
+        featureVector.append("|");
+        featureVector.append(url_place);
+        featureVector.append("|");
+        featureVector.append(retweet_count);
+        featureVector.append("|");
+        featureVector.append(text_size);
+        featureVector.append("|");
+        featureVector.append(isMobile);
+        for(int i = 0; i < t_or_f.length; i++)
+
+            featureVector.append("|" + t_or_f[i]);
+        for(int i = 0; i < fractions.length; i++)
+            featureVector.append("|" + fractions[i]);
+        return fractions.toString();
     }
 
 }
 
-/*id: long
-contributors: boolean
-created_at: <labelled in 2 dimensions: weekday vs weekend and times of day (morning, afternoon, night)
-entities_url: boolean
-entities_url_position: <labelled as beginning or end>
-entities_hashtags: int
-entities_mentions: int
-entities_media: boolean
-filter_level: <labelled as all the Twitter filter levels>
-is_reply: boolean (derived from any of the “in_reply_*” fields)
-place: boolean
-possibly_sensitive: boolean
-retweeted: int
-source: <labelled as mobile or desktop> derived from list of client names*/
