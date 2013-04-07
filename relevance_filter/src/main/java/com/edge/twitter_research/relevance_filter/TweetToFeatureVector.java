7f9bad94c26ea8a8987133eb78a92d415e8604ad
Arpan Ghosh <arpanghosh@gmail.com>
2013-04-06 22:17:19 -0400

    Added a Gatherer for generating tweet feature vectors

---
+++ relevance_filter/src/main/java/com/edge/twitter_research/relevance_filter/TweetToFeatureVector.java
@@ -0,0 +1,94 @@
+package com.edge.twitter_research.relevance_filter;
+
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.kiji.mapreduce.MapReduceJob;
+import org.kiji.mapreduce.gather.KijiGatherJobBuilder;
+import org.kiji.mapreduce.output.TextMapReduceJobOutput;
+import org.kiji.schema.KijiURI;
+
+import java.io.IOException;
+
+public class TweetToFeatureVector extends Configured {
+
+    public MapReduceJob mapReduceJob = null;
+
+    public static Logger logger =
+            Logger.getLogger(TweetToFeatureVector.class);
+
+    public TweetToFeatureVector (String outputFilePath,
+                       String log4jPropertiesFilePath,
+                       String inputTableName,
+                       float samplingRate){
+
+        PropertyConfigurator.configure(log4jPropertiesFilePath);
+
+        try{
+            Configuration hBaseConfiguration =
+                    HBaseConfiguration.addHbaseResources(new Configuration(true));
+
+            hBaseConfiguration.setFloat("sampling.rate", samplingRate);
+            hBaseConfiguration.set("mapred.textoutputformat.separator", "|");
+            hBaseConfiguration.setInt("hbase.client.scanner.caching", 1000);
+
+            KijiURI tableUri =
+                    KijiURI.newBuilder(String.format("kiji://.env/default/%s", inputTableName)).build();
+
+            this.mapReduceJob = KijiGatherJobBuilder.create()
+                    .withConf(hBaseConfiguration)
+                    .withGatherer(TweetToFeatureVectorGatherer.class)
+                    .withInputTable(tableUri)
+                    .withOutput(new TextMapReduceJobOutput(new Path(outputFilePath), 1))
+                    .build();
+        }catch (IOException ioException){
+            logger.error("IO Exception while configuring MapReduce Job", ioException);
+            System.exit(1);
+        } catch (Exception unknownException){
+            logger.error("Unknown Exception while configuring MapReduce Job", unknownException);
+            System.exit(1);
+        }
+    }
+
+
+    public static void main(String[] args){
+
+        if (args.length < 4){
+            System.out.println("Usage: TweetToFeatureVector " +
+                    "<relevance_filter_root> " +
+                    "<input_table_name> " +
+                    "<HDFS_output_file_path> " +
+                    "<sampling_rate (%)>");
+            return;
+        }
+
+        String relevanceFilterRoot = args[0];
+        String inputTableName = args[1];
+        String HDFSOutputFilePath = args[2];
+        float samplingRate = Float.parseFloat(args[3]);
+
+        TweetToFeatureVector tweetToFeatureVector =
+                new TweetToFeatureVector(HDFSOutputFilePath,
+                        relevanceFilterRoot + "/" + Constants.LOG4J_PROPERTIES_FILE_PATH,
+                        inputTableName,
+                        samplingRate);
+
+        boolean isSuccessful = false;
+        if (tweetToFeatureVector.mapReduceJob != null){
+            try{
+                isSuccessful = tweetToFeatureVector.mapReduceJob.run();
+            }catch (Exception unknownException){
+                logger.error("Unknown Exception while running MapReduce Job", unknownException);
+                System.exit(1);
+            }
+        }
+
+        String result = isSuccessful ? "Successful" : "Failure";
+        logger.info(result);
+    }
+
+}
---
+++ relevance_filter/src/main/java/com/edge/twitter_research/relevance_filter/TweetToFeatureVectorGatherer.java
@@ -0,0 +1,77 @@
+package com.edge.twitter_research.relevance_filter;
+
+
+import com.edge.twitter_research.core.SimpleTweet;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.kiji.mapreduce.gather.GathererContext;
+import org.kiji.mapreduce.gather.KijiGatherer;
+import org.kiji.schema.KijiDataRequest;
+import org.kiji.schema.KijiDataRequestBuilder;
+import org.kiji.schema.KijiRowData;
+
+import java.io.IOException;
+
+public class TweetToFeatureVectorGatherer
+        extends KijiGatherer<LongWritable, Text> {
+
+    private static final String TWEET_COLUMN_FAMILY = "tweet_object";
+    private static final String TWEET_COLUMN = "tweet";
+
+    private double threshold;
+    private TweetFeatureVectorGenerator tweetFeatureVectorGenerator;
+
+    @Override
+    public void setup(GathererContext<LongWritable, Text> context) throws IOException {
+        super.setup(context); // Any time you override setup, call super.setup(context);
+
+        Configuration conf = getConf();
+        threshold = conf.getFloat("sampling.rate", 100)/100.0;
+        tweetFeatureVectorGenerator = new TweetFeatureVectorGenerator();
+    }
+
+
+    @Override
+    public void gather(KijiRowData input, GathererContext<LongWritable, Text> context)
+            throws IOException {
+
+
+        if (Math.random() < threshold){
+
+            SimpleTweet tweet = input.getMostRecentValue(TWEET_COLUMN_FAMILY, TWEET_COLUMN);
+
+            String tweetFeatureVector = tweetFeatureVectorGenerator
+                                            .getFeatureVector(tweet);
+
+            context.write(new LongWritable(tweet.getId()),
+                            new Text(tweetFeatureVector));
+        }
+    }
+
+
+    @Override
+    public KijiDataRequest getDataRequest() {
+        // This method is how we specify which columns in each row the gatherer operates on.
+        // In this case, we need all versions of the tweet_object:tweet column.
+        final KijiDataRequestBuilder builder = KijiDataRequest.builder();
+        builder.newColumnsDef()
+                .withMaxVersions(1)
+                .add(TWEET_COLUMN_FAMILY, TWEET_COLUMN);
+        return builder.build();
+    }
+
+
+    @Override
+    public Class<?> getOutputValueClass() {
+        return CharSequence.class;
+    }
+
+    @Override
+    public Class<?> getOutputKeyClass() {
+        return LongWritable.class;
+    }
+
+
+}
+
