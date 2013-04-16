33dadfacc80d09f4da3bcae4dcd72a40e5fc27db
Arpan Ghosh <arpanghosh@gmail.com>
2013-04-16 02:46:02 -0700

    Added an 'event_detection' sub-module

---
+++ event_detection/src/main/java/com/edge/twitter_research/event_detection/Constants.java
@@ -0,0 +1,19 @@
+package com.edge.twitter_research.event_detection;
+
+
+
+public class Constants {
+
+    public static final String LOG4J_PROPERTIES_FILE_PATH = "/log4j.properties";
+
+
+    public static final String TWEET_COLUMN_FAMILY_NAME = "tweet_object";
+    public static final String TWEET_OBJECT_COLUMN_NAME = "tweet";
+    public static final String TWEET_RELEVANCE_LABEL_COLUMN_NAME = "relevance_label";
+
+    public static final String RELEVANT_RELEVANCE_LABEL = "topic-related";
+    public static final String NOT_RELEVANT_RELEVANCE_LABEL = "not-topic-related";
+    public static final String NOT_ENGLISH_RELEVANCE_LABEL = "not-english";
+
+
+}
