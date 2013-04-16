7622ad25248b227e91b93838b89f89bf6fa494b3
Arpan Ghosh <arpanghosh@gmail.com>
2013-04-16 15:56:07 -0700

    log4j.properties are not longer included in the jar for relevance_filter

--- relevance_filter/src/main/java/com/edge/twitter_research/relevance_filter/Constants.java
+++ relevance_filter/src/main/java/com/edge/twitter_research/relevance_filter/Constants.java
@@ -3,7 +3,8 @@ package com.edge.twitter_research.relevance_filter;
 
 public class Constants {
 
-    public static final String LOG4J_PROPERTIES_FILE_PATH = "/log4j.properties";
+    public static final String LOG4J_PROPERTIES_FILE_PATH =
+            "~/twitter_research/relevance-filter/log4j.properties";
 
     public static final String POS_TAGGING_MODEL_FILE_PATH = "/model.20120919";
 
