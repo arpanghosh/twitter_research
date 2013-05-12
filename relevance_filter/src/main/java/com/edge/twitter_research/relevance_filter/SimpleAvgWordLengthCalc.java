1bd7681ce62d7a19a960923aa868b433147da3bd
Arpan Ghosh <arpanghosh@gmail.com>
2013-05-11 23:21:42 -0400

    Added a simple avg word length calc

---
+++ relevance_filter/src/main/java/com/edge/twitter_research/relevance_filter/SimpleAvgWordLengthCalc.java
@@ -0,0 +1,54 @@
+package com.edge.twitter_research.relevance_filter;
+
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+public class SimpleAvgWordLengthCalc {
+
+    public static void main(String[] args){
+
+        try{
+
+            FileInputStream fstream = new FileInputStream(args[0]);
+            DataInputStream in = new DataInputStream(fstream);
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+
+            String strLine;
+
+            double relevantTotal = 0;
+            int numRelevant = 0;
+
+            double notRelevantTotal = 0;
+            int numNotRelevant = 0;
+
+            while ((strLine = br.readLine()) != null)   {
+
+                String[] tokens = strLine.split(",");
+
+                double avgWordLength = Double.parseDouble(tokens[12]);
+                int label =  Integer.parseInt(tokens[18]);
+
+                if (label == 1){
+                    relevantTotal += avgWordLength;
+                    numRelevant++;
+                }else if (label == 2){
+                    notRelevantTotal += avgWordLength;
+                    numNotRelevant++;
+
+                }
+            }
+
+            System.out.println(args[0]);
+            System.out.println("Relevant avg word length : " + relevantTotal/numRelevant);
+            System.out.println("Not Relevant avg word length : " + notRelevantTotal/numNotRelevant);
+
+
+            in.close();
+        }catch (Exception e){//Catch exception if any
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+}
