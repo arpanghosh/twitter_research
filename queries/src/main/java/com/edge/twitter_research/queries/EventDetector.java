3a2310ef23da03ec840d5bdf57c2ecb8c44a9b0d
Arpan Ghosh <arpanghosh@gmail.com>
2013-05-12 18:15:42 -0400

    bug in removing first 2 days of collection

--- queries/src/main/java/com/edge/twitter_research/queries/EventDetector.java
+++ queries/src/main/java/com/edge/twitter_research/queries/EventDetector.java
@@ -129,7 +129,7 @@ public class EventDetector
 
             newWordsLogProbabilities = newWordsLogProbabilities * ((double)entry.getValue().getSortedWordCounts().size())/1000;
 
-            if (newWordsLogProbabilities > threshold && (day != 90 || day != 91)){
+            if (newWordsLogProbabilities > threshold && day > 91){
                 StringBuilder stringBuilder = new StringBuilder(500);
                 for (WordCount wordCount : entry.getValue().getSortedWordCounts().subList(0, Math.min(100, entry.getValue().getSortedWordCounts().size()))){
                     stringBuilder.append(wordCount.getWord());
